a665bd4994f3c99a1b1f4778784a5237cec4b6b1
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2022-11-22 12:57:32 +0300

    feat: add server validation to LoginPage

--- server/dist/main.js
+++ server/dist/main.js
@@ -4,9 +4,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const PORT = process.env.PORT || 5000;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
 }
 bootstrap();
--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,7 +1,33 @@
 // объекты dto необходимы для обмена данными между подсистемами (клиент -сервре)
+import {IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator';
+
 export class CreateUserDto {
+    @IsString({
+        message: 'First name must be a string'
+    })
     readonly firstName: string;
+
+    @IsString({
+        message: 'Last name must be a string'
+    })
     readonly lastName: string;
+
+    @IsString({
+        message: 'Email must be a string'
+    })
+    @IsEmail({}, {
+        message: 'Incorrect email'
+    })
     readonly email: string;
+
+    @IsString({
+        message: 'Password must be a string'
+    })
+    @IsNotEmpty({
+        message: 'Required to fill in'
+    })
+    @MinLength(6, {
+        message: 'Password must contain at least 6 characters',
+    })
     readonly password: string;
 }
