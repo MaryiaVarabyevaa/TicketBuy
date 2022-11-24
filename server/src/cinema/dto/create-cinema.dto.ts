849f6d1434fba49e6ab6d6ad647a46f932b88c95
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2022-11-24 12:28:37 +0300

    feat: add server validation to CinemaForm

--- server/src/cinema/dto/create-cinema.dto.ts
+++ server/src/cinema/dto/create-cinema.dto.ts
@@ -1,4 +1,11 @@
 // объекты dto необходимы для обмена данными между подсистемами (клиент -сервре)
+import {IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator';
 export class CreateCinemaDto {
+    @IsNotEmpty({
+        message: 'Required to fill in'
+    })
+    @IsString({
+        message: 'Cinema name must be a string'
+    })
     readonly name: string;
 }
