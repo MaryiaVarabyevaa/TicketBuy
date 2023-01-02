92561737ea106b0e49b3662f2cc82efec1b06664
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-01-02 17:23:49 +0300

    feat: add modal to SessionDataTable component before deleting session

--- client/src/pages/dashboard/DataTable/session/SessionDataTable.tsx
+++ client/src/pages/dashboard/DataTable/session/SessionDataTable.tsx
@@ -15,7 +15,7 @@ import SaveIcon from "@mui/icons-material/Save";
 import CancelIcon from "@mui/icons-material/Close";
 import EditIcon from "@mui/icons-material/Edit";
 import Box from "@mui/material/Box";
-import {Typography} from "@mui/material";
+import {Button, Modal, Typography} from "@mui/material";
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import {getAllFilms} from "../../../../http/filmAPI";
 import {handleRowEditStart} from "../handleFunctions";
@@ -30,6 +30,18 @@ import {EditToolbar} from "./EditToolbar";
 import {pricePreProcessEditCellProps} from "./validation";
 import {renderEditCell} from "../CellEditInputCell";
 
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
 const SessionDataTable = () => {
     const [rows, setRows] = useState<ISession[]>([]);
     const [isClicked, setIsClicked] = useState(false);
@@ -39,6 +51,8 @@ const SessionDataTable = () => {
     const [cinemaName, setCinemaName] = useState<string[]>([]);
     const [films, setFilms] = useState<any[]>([]);
     const [cinema, setCinema] = useState<any[]>([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [sessionId, setSessionId] = useState<number | null>(null);
 
     const getData = async () => {
         const films = await getAllFilms();
@@ -96,9 +110,16 @@ const SessionDataTable = () => {
     };
 
     const handleDeleteClick = (id: GridRowId) => async () => {
-        setIsClicked(!isClicked);
-        await deleteSession(id);
+        setOpenModal(true);
+        setSessionId(+id);
     };
+    const handleModalDeleteClick = async () => {
+        if (sessionId) {
+            await deleteSession(sessionId);
+            setIsClicked(!isClicked);
+            setOpenModal(false);
+        }
+    }
 
     const handleCancelClick = (id: GridRowId) => () => {
         setRowModesModel({
@@ -207,6 +228,19 @@ const SessionDataTable = () => {
 
     return (
         <>
+            <Modal
+                open={openModal}
+                onClose={() => setOpenModal(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Are you sure you want to delete this film?
+                    </Typography>
+                    <Button onClick={handleModalDeleteClick}>Delete</Button>
+                </Box>
+            </Modal>
             <Box
                 sx={{
                     height: 422,
